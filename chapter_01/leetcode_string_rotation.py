cd7cab432f9b87edfa1d4703e6fd6d134410d820
root <root@AC929999.localdomain>
2022-09-30 13:22:12 +0700

    update

---
+++ chapter_01/leetcode_string_rotation.py
@@ -0,0 +1,5 @@
+"""
+problem: check a string is another rotation of another string.
+analyze: 
+    #pick a point in string and lift the left to the right side of string. Example: abcde -> bcdea
+"""
