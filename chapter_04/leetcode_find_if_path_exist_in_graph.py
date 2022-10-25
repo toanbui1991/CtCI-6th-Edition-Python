17a35e7c5832fba034491ad59df9e6e5091cae5f
toanbui1991 <toanbui1991@gmail.com>
2022-10-25 17:24:00 +0700

    update

--- chapter_04/leetcode_find_if_path_exist_in_graph.py
+++ chapter_04/leetcode_find_if_path_exist_in_graph.py
@@ -1,48 +1,68 @@
 from typing import List
 from collections import defaultdict, deque
 """
-problem: given bi-direction graph, 
+problem: given bi-direction graph with n is the number of nodes, edges is path between nodes.
+    start node, end node, check that their is a path between start node and end node
+
+breadth first search:
+    - breadth first search explore graph in layer fashion.
+    - use queue to keep track of what node to be processed next. nodes in queue is the child of child node
+    - use visited nodes to check nodes which have been visited. If nodes have been finished continue
+    - remove current when add all node which is child of the current node
 """
 
 from collections import defaultdict, deque
+#breadth first search solution
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
-        #inital check for special case
-        if start == end:
-            return True
-        #using defaultdict to build graph
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         graph = defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
+        
+        # Create undirected graph
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
             
-        # return self.bfs(start, end, graph)
-        return self.dfs(start, end, set(), graph)
+        queue = deque([source])
         
-    def bfs(self, start, end, graph):
-        
-        visited = set() #to keep track of nodes have been visited
-        queue = deque([start]) #queue to keep track of node have ben check
-        while queue: #for each node
-            curr = queue.popleft()
-            for child in graph[curr]:
-                if child in visited:
-                    continue
-                visited.add(child) #visited all child nodes
-                if child == end:
-                    return True
-                for e in graph[child]: #for each child node, get child of that child node to append to queue
-                    queue.append(e)
-        return False
-    
-    def dfs(self, node, end, visited, graph):
-        if node == end:
-            return True
-        if node not in visited:
+        visited = set()
+        while queue:
+            node = queue.popleft()
+            
             visited.add(node)
-            for child in graph[node]:
-                res = self.dfs(child, end, visited, graph)
-                if res:
-                    return True
+            
+            if node == destination:
+                return True
+            
+            for neighbor in graph[node]:
+                if neighbor in visited:
+                    continue
+                queue.append(neighbor)
+
+#depth first search solution
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+        
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+        
+        visited = set()
+        
+        def dfs(node, dest):
+            if node == dest:
+                return True
+                
+            visited.add(node)
+            
+            all_paths = []
+            for neighbor in graph[node]:
+                if neighbor in visited:
+                    continue
+                all_paths.append(dfs(neighbor, dest))
+			# return true if any of the paths were able to find the value
+            return any(all_paths)
+        
+        return dfs(source, destination)
             
             
